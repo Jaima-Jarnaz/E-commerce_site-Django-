218f7ea09fda07ac92dbe352b59f022ab1648907
Mim <jaima.jarnaz1234@gmail.com>
2020-10-10 09:31:18 +0600

    10102020commit

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from store import views
-from .views import index,signup
+from .views import Signup,Login,Index,Cart,logout,Checkout,Orders
 urlpatterns=[
-    path('',index),
-    path('signup',views.signup,name='signup')
+    path('',Index.as_view(),name='homepage'),
+    path('signup',Signup.as_view(),name='signup'),
+    path('login',Login.as_view(),name='login'),
+    path('cart',Cart.as_view(),name='cart'),
+    path('checkout',Checkout.as_view(),name='checkout'),
+     path('orders',Orders.as_view(), name='orders'),
+    path('logout',views.logout, name='logout')
 ]
